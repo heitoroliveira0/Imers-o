455f3ff275f2a9fed33b52923cb76dff15593ef3
heitoroliveira0 <ferreiraheitor1204@gmail.com>
2024-09-04 19:25:09 -0300

    feat: inicializando projeto

---
+++ dados.js
@@ -0,0 +1,21 @@
+
+    let dados = [
+        {
+          titulo: "Rayssa Leal",
+          descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce. Mais jovem medalhista olímpica da história do Brasil, ela é um símbolo de superação e inspiração para jovens skatistas.",
+          link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        },
+        {
+          titulo: "Beatriz Souza",
+          descricao: "Beatriz Souza é uma judoca brasileira, campeã olímpica nos Jogos Olímpicos de Paris 2024. Sua conquista histórica representou um marco para o judô feminino brasileiro.",
+          link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+        },
+        {
+          titulo: "Rebeca Andrade",
+          descricao: "Rebeca Andrade é uma ginasta brasileira, conhecida por sua versatilidade e carisma. Medalhista olímpica, ela é um dos maiores nomes da ginástica artística brasileira.",
+          link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+        }
+      
+];
+
+ 
